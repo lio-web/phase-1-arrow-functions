c01535ff9d5d3ade5ad64d3eb1c5373c08c4150e
Leonard <leonard.muhati@student.moringaschool.com>
2024-06-29 10:37:46 +0300

    my arrow functions

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide=(a,b)=>a/b;
+  divide(2000,100);
+const square = x=>(x**2);
+ square(2)
+const add = (a,b) =>a+b;
+ add(3,4);
+ const sum =(a, b) => {
+  console.log(`Adding ${a}`);
+  console.log(`Adding ${b}`);
+  return a + b;
+}
+sum(3,4); //=> 7
